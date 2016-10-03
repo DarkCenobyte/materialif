e723f209ed5cfeda149af38febe8965f23c4c2a9
Olivier Seror-Droin <161189@supinfo.com>
2016-10-03 23:31:49 +0200

    Test with vars in template.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -10,6 +10,9 @@ class IndexController extends BaseController
   public function index()
   {
     //echo "Hello, World...";
-    $this->renderer->render("index");
+    $this->renderer->render("index", [
+      "customName" => "DarkCenobyte",
+      "customRank" => "Master"
+    ]);
   }
 }
--- views/index/index.tpl
+++ views/index/index.tpl
@@ -4,5 +4,7 @@
   </head>
   <body>
     <h1>Hello, World...</h1>
+    <h2>YourName: {$customName}</h2>
+    <h3>You're a {$customRank}</h3>
   </body>
 </html>
