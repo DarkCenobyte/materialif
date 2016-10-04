5957480185eb8790be7133a69a9fcfa5dc54cb69
Olivier Seror-Droin <161189@supinfo.com>
2016-10-04 09:54:04 +0200

    Base model code

---
+++ models/BaseModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class BaseModel extends Eloquent
+{
+  function __construct()
+  {
+
+  }
+  
+}
---
+++ models/Categories.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Models;
+
+class Categories extends BaseModel
+{
+
+}
