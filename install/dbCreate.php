0dadd5edce0c662942b8b7b9d361dadd9650cb90
Olivier Seror-Droin <darkcenobyte@gmail.com>
2016-10-05 16:26:06 +0200

    Force InnoDB engine and fix foreign key error.

--- install/dbCreate.php
+++ install/dbCreate.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Schema\Blueprint;
 
 //Create config table
 Capsule::schema()->create('config', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->string('param_name')->unique();
     $table->enum('param_type', array('string', 'integer'))->default('string');
@@ -13,6 +14,7 @@ Capsule::schema()->create('config', function (Blueprint $table) {
 
 //Create extensions table
 Capsule::schema()->create('extensions', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->string('name');
     $table->string('author_name');
@@ -21,12 +23,14 @@ Capsule::schema()->create('extensions', function (Blueprint $table) {
 
 //Create ranks table
 Capsule::schema()->create('ranks', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->string('name')->unique();
 });
 
 //Create categories table
 Capsule::schema()->create('categories', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->string('name');
     $table->text('description')->nullable();
@@ -34,24 +38,26 @@ Capsule::schema()->create('categories', function (Blueprint $table) {
 
 //Create rights levels table
 Capsule::schema()->create('rights_levels', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->boolean('can_read');
     $table->boolean('can_write');
     $table->boolean('can_moderate');
     $table->boolean('is_admin')->default(0);
-    $table->integer('rank_id');
+    $table->integer('rank_id')->unsigned();
     $table->foreign('rank_id')->references('id')->on('ranks');
-    $table->integer('apply_on_cat_id')->nullable();
+    $table->integer('apply_on_cat_id')->unsigned()->nullable();
     $table->foreign('apply_on_cat_id')->references('id')->on('categories');
 });
 
 //Create users table
 Capsule::schema()->create('users', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->string('username')->unique();
     $table->string('email')->unique();
     $table->string('password');
-    $table->integer('rank_id');
+    $table->integer('rank_id')->unsigned();
     $table->foreign('rank_id')->references('id')->on('ranks');
     $table->string('first_name')->nullable();
     $table->string('last_name')->nullable();
@@ -61,22 +67,24 @@ Capsule::schema()->create('users', function (Blueprint $table) {
 
 //Create threads table
 Capsule::schema()->create('threads', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->string('title');
-    $table->integer('author_id');
+    $table->integer('author_id')->unsigned();
     $table->foreign('author_id')->references('id')->on('users');
-    $table->integer('category_id');
+    $table->integer('category_id')->unsigned();
     $table->foreign('category_id')->references('id')->on('categories');
     $table->timestamps();
 });
 
 //Create posts table
 Capsule::schema()->create('posts', function (Blueprint $table) {
+    $table->engine = 'InnoDB';
     $table->increments('id');
     $table->text('content');
-    $table->integer('author_id');
+    $table->integer('author_id')->unsigned();
     $table->foreign('author_id')->references('id')->on('users');
-    $table->integer('thread_id');
+    $table->integer('thread_id')->unsigned();
     $table->foreign('thread_id')->references('id')->on('threads');
     $table->timestamps();
 });
