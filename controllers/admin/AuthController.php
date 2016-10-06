59b3b4e78cc600b47f98781bc4ceec84f62b201b
Olivier Seror-Droin <161189@supinfo.com>
2016-10-06 10:15:22 +0200

    Add admin login, with PHP hash/rehash/verify implementation.

--- controllers/admin/AuthController.php
+++ controllers/admin/AuthController.php
@@ -14,6 +14,23 @@ class AuthController extends BaseController
 
   public function auth($username, $password)
   {
-    User::where('ranks_id', Rank::ADMIN)->where('username', $username)->get();
+    $user = User::where('ranks_id', Rank::ADMIN)
+      ->where('username', $username)
+      ->first();
+
+    if ($user && password_verify($user->password, $hash)) {
+      if (password_needs_rehash($hash, PASSWORD_DEFAULT)) {
+        $user->password = password_hash($password, PASSWORD_DEFAULT);
+        $user->save();
+      }
+      $this->login($user->id, $user->username);
+    }
   }
+
+  private function login($id, $username)
+  {
+    $_SESSION['userid'] = $id;
+    $_SESSION['username'] = $username;
+  }
+
 }
