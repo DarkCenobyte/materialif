c4409b8681a8dbcc1e1e93c291b59748d1b50ba4
Olivier Seror-Droin <161189@supinfo.com>
2016-10-09 21:25:40 +0200

    Rewrite URL php-side instead of .htaccess

--- admin/index.php
+++ admin/index.php
@@ -3,9 +3,28 @@
 require_once "../vendor/autoload.php";
 require_once "../config/config.php";
 
-$class = "Controllers\Admin\\" . (isset($_GET['c']) ? ucfirst($_GET['c']) . 'Controller' : 'IndexController');
-$target = $_GET['t'] ?? "index";
-$params = $_GET['p'] ?? $_POST['p'] ?? null;
+$target = "index";
+$params = [];
+
+if (URL_REWRITER_ACTIVATED) {
+  //pretty url support
+  if (isset($_GET['rd'])) {
+    $url = explode('/', $_GET['rd']);
+    $class = "Controllers\Admin\\" . ucfirst($url[0]) . 'Controller';
+    $target = $url[1] ?? "index";
+    for ($i=2; $i < count($url); $i+=2) {
+      $params[$url[$i]] = $url[$i+1] ?? null;
+    }
+  } else {
+    $class = "Controllers\Admin\IndexController";
+  }
+} else {
+  //classic params support
+  $class = "Controllers\\" . (isset($_GET['c']) ? ucfirst($_GET['c']) . 'Controller' : 'IndexController');
+  $target = $_GET['t'] ?? "index";
+  $params = $_GET['p'] ?? $_POST['p'] ?? null;
+}
+
 if (class_exists($class, true)) {
   new $class($target, $params);
 } else {
