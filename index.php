791576f1ebf71b7a1cad8c946663d846f5ea0367
Olivier Seror-Droin <darkcenobyte@gmail.com>
2016-10-09 18:21:34 +0200

    debug stuff (remove later)

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once "config/config.php";
 $class = "Controllers\\" . (isset($_GET['c']) ? ucfirst($_GET['c']) . 'Controller' : 'IndexController');
 $target = $_GET['t'] ?? "index";
 $params = $_GET['p'] ?? $_POST['p'] ?? null;
+var_dump($_SERVER['QUERY_STRING']);
+var_dump($_GET);die;
 if (class_exists($class, true)) {
   new $class($target, $params);
 } else {
