77790c0d276f5bbd047d7814f6c67afea7948d9e
Olivier Seror-Droin <161189@supinfo.com>
2016-10-10 21:48:06 +0200

    Fill Category and User Controllers basic functions

--- controllers/admin/CategoryController.php
+++ controllers/admin/CategoryController.php
@@ -5,13 +5,49 @@ namespace Controllers\Admin;
 /**
  * Categories settings page
  */
+use Models\Category;
+
 class CategoryController extends BaseController
 {
-  public function index()
+  public function index($page = 1)
   {
+    //get list
+    $categories = Category::skip(($page - 1) * 10)->take(10)->get();
+    $pageCount = ceil(Category::count() / 10);
+
     $this->renderer->render("index", [
-      "stat_posts" => 1,
-      "stat_users" => 1
+      "categoriesList"   => $categories,
+      "categoriesPCount" => $pageCount,
+      "currentPage" => $page
     ]);
+  }
+
+  public function add($name, $description = null, $parent = null)
+  {
+    $category = new Category();
+    $category->name = $name;
+    $category->description = $description;
+    $category->parent = $parent;
+    $category->save();
+
+    $this->redirect->to("category");
+  }
+
+  public function edit($id, $name = null, $description = null, $parent = null)
+  {
+    $category = Category::find($id);
+    $category->name = $name;
+    $category->description = $description;
+    $category->parent = $parent;
+    $category->save();
+
+    $this->redirect->to("category");
+  }
+
+  public function remove($id)
+  {
+    Category::destroy($id);
+
+    $this->redirect->to("category");
   }
 }
--- controllers/admin/UserController.php
+++ controllers/admin/UserController.php
@@ -5,13 +5,63 @@ namespace Controllers\Admin;
 /**
  * Users settings page
  */
+use Models\User;
+
 class UserController extends BaseController
 {
-  public function index()
+  public function index($page = 1)
   {
+    //get list
+    $users = User::skip(($page - 1) * 10)->take(10)->get();
+    $pageCount = ceil(User::count() / 10);
+
     $this->renderer->render("index", [
-      "stat_posts" => 1,
-      "stat_users" => 1
+      "usersList"   => $users,
+      "usersPCount" => $pageCount,
+      "currentPage" => $page
     ]);
+  }
+
+  public function add($username, $email, $password, $rank = Rank::REGISTERED,
+                      $avatar = null, $firstName = null, $lastName = null,
+                      $birthdate = null)
+  {
+    $user = new User();
+    $user->username = $username;
+    $user->email = $email;
+    $user->password = $parent;
+    $user->rank = $rank;
+    $user->avatar = $avatar;
+    $user->first_name = $firstName;
+    $user->last_name = $lastName;
+    $user->birthdate = $birthdate;
+    $user->save();
+
+    $this->redirect->to("user");
+  }
+
+  public function edit($id, $username, $email, $password, $rank,
+                       $avatar, $firstName, $lastName,
+                       $birthdate)
+  {
+    $user = User::find($id);
+    $user->username = $username;
+    $user->email = $email;
+    $user->password = $parent;
+    $user->rank = $rank;
+    $user->avatar = $avatar;
+    $user->first_name = $firstName;
+    $user->last_name = $lastName;
+    $user->birthdate = $birthdate;
+    $user->save();
+
+    $this->redirect->to("user");
+  }
+
+  public function remove($id)
+  {
+    User::destroy($id);
+
+    $this->redirect->to("user");
   }
 }
